749c532b7145d47cc852ed1ca58c8f519273af63
Piu Piuson <piu@piuson.com>
2024-02-22 20:05:24 +0200

    Try to update water temp

--- esphome/components/daikin_hpc/daikin_hpc.cpp
+++ esphome/components/daikin_hpc/daikin_hpc.cpp
@@ -7,6 +7,8 @@ namespace daikin_hpc {
 
 static const char *const TAG = "daikin_hpc";
 
+static constexpr uint8_t MODBUS_CMD_READ_REGISTERS = 4;
+
 void DaikinHpcClimate::setup() {
   waterTemperature_->set_icon("mdi:thermometer");
   waterTemperature_->set_unit_of_measurement("°C");
@@ -15,9 +17,13 @@ void DaikinHpcClimate::setup() {
   waterTemperature_->set_entity_category(EntityCategory::ENTITY_CATEGORY_DIAGNOSTIC);
 }
 
-void DaikinHpcClimate::on_modbus_data(const std::vector<uint8_t> &data) {}
+void DaikinHpcClimate::on_modbus_data(const std::vector<uint8_t> &data) {
+  for (const auto &b : data) {
+    ESP_LOGI("0x%02X", b);
+  }
+}
 
-void DaikinHpcClimate::update() { waterTemperature_->publish_state(2); }
+void DaikinHpcClimate::update() { this->send(MODBUS_CMD_READ_REGISTERS, Register::WaterTemperature, 1); }
 
 void DaikinHpcClimate::dump_config() {
   ESP_LOGCONFIG(TAG, "DaikinHpcClimate:");

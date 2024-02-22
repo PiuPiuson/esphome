c6863496495a9e4b98d32dab925eb4bf7af9b511
Piu Piuson <piu@piuson.com>
2024-02-22 15:28:37 +0200

    Change config schema

--- esphome/components/daikin_hpc/climate.py
+++ esphome/components/daikin_hpc/climate.py
@@ -16,7 +16,7 @@ CONFIG_SCHEMA = cv.Schema({
 
 
 
-# async def to_code(config):
-#     var = cg.new_Pvariable(config[CONF_ID])
-#     await cg.register_component(var, config)
-#     await modbus.register_modbus_device(var, config)
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await modbus.register_modbus_device(var, config)

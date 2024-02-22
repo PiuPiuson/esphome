af2c7ae5d5c892dabc662c0acfce1a687a95204a
Piu Piuson <piu@piuson.com>
2024-02-22 15:33:37 +0200

    change config schema

--- esphome/components/daikin_hpc/climate.py
+++ esphome/components/daikin_hpc/climate.py
@@ -1,6 +1,6 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import modbus
+from esphome.components import modbus, climate
 from esphome.const import CONF_ID
 
 AUTO_LOAD = ["modbus"]
@@ -10,10 +10,11 @@ DaikinBrcClimate = daikin_hpc_ns.class_("DaikinHpcClimate")
 
 CONF_USE_FAHRENHEIT = "use_fahrenheit"
 
-CONFIG_SCHEMA = cv.Schema({
+CONFIG_SCHEMA = climate.CLIMATE_CONTROL_ACTION_SCHEMA.extend(
+    {
         cv.Optional(CONF_USE_FAHRENHEIT, default=False): cv.boolean,
-}).extend(modbus.modbus_device_schema(0x01))
-
+    }
+).extend(modbus.modbus_device_schema(0x01))
 
 
 async def to_code(config):
